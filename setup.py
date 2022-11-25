f3a69508e51a285a0d739de6a1be89846cda5df2
Jean-Sébastien Gosselin <jean-sebastien.gosselin@outlook.com>
2022-11-25 15:21:39 -0500

    Update the setup

--- setup.py
+++ setup.py
@@ -1,25 +1,18 @@
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-import numpy
+from setuptools import setup
+from segdio import __version__, __project_url__
 
-setup(
-      name = 'SEGD',
-      version = '0.a1',
-      description = 'A Python3 reader for SEG-D rev3.1 binary data.',
-      url = 'https://github.com/drsudow/SEG-D.git',
-      author = 'Mattias Südow',
-      author_email = 'mattias@sudow.com',
-      license = 'MIT',
-      classifiers = [
-                     'Development Status :: 3 -Aplha',
-                     'Intended Audience :: Science/Research',
-                     'License :: OSI Approved :: MIT License',
-                     'Topic :: Scientific/Engineering :: Information Analysis',
-                     'Programming Language :: Python :: 3.5',
-                     ],
-      keywords = 'seismic SEGD',
-      packages = ['SEGD'],
-      install_requires = ['cython','numpy','datetime'],
-      ext_modules = cythonize([Extension('SEGD.read_traces',['SEGD/read_traces.pyx']
-                               ,include_dirs=[numpy.get_include()])])
+"""Installation script """
+
+setup(name='segdio',
+      version=__version__,
+      description='A Python3 reader for SEG-D binary data.',
+      url=__project_url__,
+      license='MIT',
+      classifiers=[
+          'Intended Audience :: Science/Research',
+          'License :: OSI Approved :: MIT License',
+          "Operating System :: OS Independent",
+          'Programming Language :: Python :: 3',
+          ],
+      packages=['segdio'],
       )
